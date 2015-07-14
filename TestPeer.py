7e1fc20aa091670fa2fbca9de2859c382438b933
thor <saintthor@gmail.com>
2015-07-14 21:57:57 +0800

    TestPeer

---
+++ TestPeer.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 14 21:49:20 2015
+
+@author: thor
+"""
+
+from peerforum import PeerForum
+from bottle import route, run, post, debug, request, static_file
+
+from sqlitedb import SqliteDB, InitDB
+from protocol import PFPMessage
+
+
+
+class TestPeerForum( PeerForum ):
+    ""
+    pass
+
+if __name__ == '__main__':
+    InitDB( './test.db' )
+    PFPMessage.Init()
+    PeerForum.ChkEnv()
+    debug( True )
+    run( host = '0.0.0.0', port = 8001, reloader = True )
+    
